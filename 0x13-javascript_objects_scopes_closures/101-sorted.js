c3d5348e425417ef4fcf97223fef9a761879ec39
Clarenced1 <clarencegreat@gmail.com>
2023-06-14 10:51:34 -0700

    101 sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,12 +3,12 @@ const dictInput = require('./101-data').dict;
 const outDict = {};
 
 for (const key in dictInput) {
-	const ocurr = dictInput[key];
-	outDict[ocurr] = [];
-	for (const keys in dictInput) {
-		if (dictInput[keys] === ocurr) {
-			outDict[ocurr].push(keys);
-		}
-	}
+  const ocurr = dictInput[key];
+  outDict[ocurr] = [];
+  for (const keys in dictInput) {
+    if (dictInput[keys] === ocurr) {
+      outDict[ocurr].push(keys);
+    }
+  }
 }
 console.log(outDict);
